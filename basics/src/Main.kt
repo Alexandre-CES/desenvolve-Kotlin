858a5e9575e07abbb3484cf4c849a3dc88b95168
Alexandre <alexandreces666@gmail.com>
2025-08-28 13:27:27 -0300

    feat: operators and conditions

--- basics/src/Main.kt
+++ basics/src/Main.kt
@@ -1,17 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 fun main() {
-    val name = "Kotlin"
-    var age = 12; age = 11
-    println(age)
+    val name:String = "Kotlin"
+    val age:Int = 12
+    var payment:Double = 1500.30
 
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
-
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    for (i in 1..3) {
+        payment += 15.5
+        println("i = $i | payment = $payment")
     }
+
+    fun sum(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    println("Hello, " + name + "!")
+    println("sum(1,2) : ${sum(age,3)}")
+    println(age <= payment)
 }
