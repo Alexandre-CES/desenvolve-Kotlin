95111fa476802949523857f34b4d8bc8ea170e79
Alexandre <alexandreces666@gmail.com>
2025-08-28 13:41:31 -0300

    feat: loops

--- basics/src/Main.kt
+++ basics/src/Main.kt
@@ -16,4 +16,34 @@ fun main() {
     println("Hello, " + name + "!")
     println("sum(1,2) : ${sum(age,3)}")
     println(age <= payment)
+
+    if(age <= 12){
+        println("child")
+    }else if(age > 12 && age < 18){
+        println("teenager")
+    }else{
+        println("adult")
+    }
+
+    val day = 3
+    val dayName = when (day){
+        1 -> "Sunday"
+        2 -> "Monday"
+        3 -> "Tuesday"
+        4 -> "Wednesday"
+        5 -> "Thursday"
+        6 -> "Friday"
+        7 -> "Saturday"
+        else -> "invalid"
+    }
+
+    println(dayName)
+
+    var counter = 0
+    while (counter < day){
+        println(counter)
+        counter++
+    }
+
+
 }
