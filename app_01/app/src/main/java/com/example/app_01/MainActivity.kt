58322b3672640d9ba4cb207acdac18669ef151b9
Alexandre <alexandreces666@gmail.com>
2025-08-31 09:26:43 -0300

    feat: Layouts

--- app_01/app/src/main/java/com/example/app_01/MainActivity.kt
+++ app_01/app/src/main/java/com/example/app_01/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.app_01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -11,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -24,17 +26,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MinhaTela() {
-    Column( //Organiza os componentes verticalmente.
+    Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color.LightGray)
             .padding(16.dp)
     ){
-        Text(text = "Olá, Jetpack Compose!", style = MaterialTheme.typography.headlineMedium)
-        //Exibe um texto.
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /* Ação do botão */ }){
-            //Cria um botão interativo.
-            Text(text = "Clique Aqui")
+        Column( //Organiza os componentes verticalmente.
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ){
+            Text(
+                text = "Olá, Jetpack Compose!",
+                style = MaterialTheme.typography.headlineMedium
+            )
+            //Exibe um texto.
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = { /* Ação do botão */ }){
+                //Cria um botão interativo.
+                Text(text = "Clique Aqui")
+            }
         }
     }
+
 }
